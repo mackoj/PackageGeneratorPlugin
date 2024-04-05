c10d6180afc977534a34b40c998621968036d404
Jeffrey Macko <jeffrey.macko@decathlon.com>
2024-04-05 09:16:27 +0200

    Bump package

--- Package.swift
+++ Package.swift
@@ -14,8 +14,13 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "package-generator-cli",
+<<<<<<< Updated upstream
       url: "https://github.com/mackoj/PackageGeneratorCLI/releases/download/0.4.0/package-generator-cli-arm64-apple-macosx.artifactbundle.zip",
       checksum: "8087731a742d7b834cdf33eb07ce11fcc054fb90c3b4f919b6080d54878d0dd5"
+=======
+      url: "https://github.com/mackoj/PackageGeneratorCLI/releases/download/0.4.2/package-generator-cli-arm64-apple-macosx.artifactbundle.zip",
+      checksum: "ddf169bfe8b9260d40069671b8e7282b2655679d2b404d080af2a3935025fb7f"
+>>>>>>> Stashed changes
     ),
     .plugin(
       name: "Package Generator",
