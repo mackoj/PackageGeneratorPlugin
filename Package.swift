88ad662b0819b94e7932a810f17211be0b026be5
Jeffrey Macko <jeffrey.macko@decathlon.com>
2024-05-03 01:43:38 +0200

    feat: prepare for splittingh

--- Package.swift
+++ Package.swift
@@ -11,12 +11,19 @@ let package = Package(
   products: [
     .plugin(name: "PackageGenerator", targets: ["Package Generator"]),
   ],
+//  dependencies: [
+//    .package(path: "/Users/mac-JMACKO01/Developer/PackageGeneratorCLI"),
+//  ],
   targets: [
     .binaryTarget(
       name: "package-generator-cli",
-      url: "https://github.com/mackoj/PackageGeneratorCLI/releases/download/0.4.2/package-generator-cli-arm64-apple-macosx.artifactbundle.zip",
-      checksum: "ddf169bfe8b9260d40069671b8e7282b2655679d2b404d080af2a3935025fb7f"
+      path: "../PackageGeneratorCLI/package-generator-cli-arm64-apple-macosx.artifactbundle.zip"
     ),
+//    .binaryTarget(
+//      name: "package-generator-cli",
+//      url: "https://github.com/mackoj/PackageGeneratorCLI/releases/download/0.4.2/package-generator-cli-arm64-apple-macosx.artifactbundle.zip",
+//      checksum: "ddf169bfe8b9260d40069671b8e7282b2655679d2b404d080af2a3935025fb7f"
+//    ),
     .plugin(
       name: "Package Generator",
       capability: .command(
@@ -29,7 +36,8 @@ let package = Package(
         ]
       ),
       dependencies: [
-        .target(name: "package-generator-cli")
+//        .product(name: "package-generator-cli", package: "PackageGeneratorCLI"),
+        .target(name: "package-generator-cli"),
       ],
       path: "Plugins/PackageGenerator"
     ),
