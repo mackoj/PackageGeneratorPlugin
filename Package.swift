001bfeb6fdaedbd4e5e6fbfd6aaa32bf1caddc46
Jeffrey Macko <jeffrey.macko@decathlon.com>
2024-05-14 12:37:45 +0200

    update cli version

--- Package.swift
+++ Package.swift
@@ -14,13 +14,13 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "package-generator-cli",
-      url: "https://github.com/mackoj/PackageGeneratorCLI/releases/download/0.5.1/package-generator-cli-arm64-apple-macosx.artifactbundle.zip",
-      checksum: "758615aeda296df2870341b311eaac5deb518a2f9a2e1c438520901f96d0fa74"
+      url: "https://github.com/mackoj/PackageGeneratorCLI/releases/download/0.5.2/package-generator-cli-arm64-apple-macosx.artifactbundle.zip",
+      checksum: "b374072f0c4ce56761be1cc00469b14f8c415e06758bb82f8485b17768f06b0f"
     ),
-//    .binaryTarget(
-//      name: "package-generator-cli",
-//      path: "../PackageGeneratorCLI/package-generator-cli-arm64-apple-macosx.artifactbundle.zip"
-//    ),
+    // .binaryTarget(
+    //   name: "package-generator-cli",
+    //   path: "../PackageGeneratorCLI/package-generator-cli-arm64-apple-macosx.artifactbundle.zip"
+    // ),
     .plugin(
       name: "Package Generator",
       capability: .command(
